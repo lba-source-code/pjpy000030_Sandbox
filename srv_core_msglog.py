27d719964b0584e1096570e4bc2b045b91abee42
Luis Benavides Andrade <luis.benavides.andrade@gmail.com>
2022-02-16 10:40:34 +0100

    DSSi comment: update srv core_msglog

--- srv_core_msglog.py
+++ srv_core_msglog.py
@@ -1,4 +1,5 @@
 """DSSi comment: stantdard package"""
+from colorama import Fore, Back, Style
 
 """DSSi comment: custom package"""
 
@@ -6,13 +7,13 @@
 class Cl_msglog:
 
     def mt_msglog_start(self, msg="ini", description="init"):
-        print(f'\nDSSi log START {msg}: {description}\n')
+        print(Back.CYAN + f'\nDSSi log START {msg}: {description}\n')
     
-    def mt_msglog_start(self, msg="ini", description="init"):
-        print(f'\nDSSi log {msg}: {description}\n')
+    def mt_msglog(self, msg="ini", description="init"):
+        print(Back.MAGENTA + f'\nDSSi log {msg}: {description}\n')
 
-    def mt_msglog_start(self, msg="ini", description="init"):
-        print(f'\nDSSi log ERROR {msg}: {description}\n')
+    def mt_msglog_error(self, msg="ini", description="init"):
+        print(Back.RED + f'\nDSSi log ERROR {msg}: {description}\n')
 
-    def mt_msglog_start(self, msg="ini", description="init"):
-        print(f'\nDSSi log FINISH {msg}: {description}\n')
+    def mt_msglog_finished(self, msg="ini", description="init"):
+        print(Back.GREEN + f'\nDSSi log FINISH {msg}: {description}\n')
